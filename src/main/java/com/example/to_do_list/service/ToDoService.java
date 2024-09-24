dffcde4eb666e3a113a3fb4ffab99084cda71441
“poelri” <qwer7755asdf@gmail.com>
2024-09-24 15:34:46 +0900

    build

--- src/main/java/com/example/to_do_list/service/ToDoService.java
+++ src/main/java/com/example/to_do_list/service/ToDoService.java
@@ -1,33 +1,54 @@
 package com.example.to_do_list.service;
 
-import com.example.to_do_list.repository.ToDoDao;
+
+import com.example.to_do_list.dto.TodoDto;
+import com.example.to_do_list.repository.TodoRepository;
+import com.example.to_do_list.entity.Todo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
-
 
 @Service
 public class ToDoService {
-
     @Autowired
-    private ToDoDao toDoDao;
+    private TodoRepository repository;
 
-    //추가 수정
-    public void addList(String todo){
-        toDoDao.add(todo);
+    // 추가
+    public TodoDto save(TodoDto dto) {
+        Todo todo = repository.save(new Todo(
+                dto.getContent()));
+        return new TodoDto(todo.getId(), todo.getContent());
     }
 
-    public List<Object> getList(){
-        return toDoDao.getToDoList();
+    // 조회
+    public List<TodoDto> getAll() {
+        List<Todo> todolist = repository.findAllByOrderByIdDesc();
+        List<TodoDto> list = new ArrayList<>();
+        for (Todo todo : todolist) {
+            list.add(new TodoDto(todo.getId(), todo.getContent()
+            ));
+        }
+        return list;
     }
 
-    public void edit(String ori, String todo) {
-        toDoDao.edit(ori, todo);
+    // 수정
+    public TodoDto update(Long id, TodoDto dto) {
+        Todo todo = repository.findById(id).orElse(null);
+        if (todo != null) {
+            todo.setContent(dto.getContent());
+
+            Todo updatedTodo = repository.save(todo);
+            return new TodoDto(updatedTodo.getId(), updatedTodo.getContent());
+        }
+        return null;
     }
 
-    public void deleteList(String todo){
-        toDoDao.delete(todo);
-    }
 
+
+    // 삭제
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
 }
